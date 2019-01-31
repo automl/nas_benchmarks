41f45fe23192dd5816782dda0b14053ea971141a
David-Elias Künstle <dekuenstle@users.noreply.github.com>
2019-01-31 12:39:44 +0100

    Specify nasbench as dependency link requirement

--- setup.py
+++ setup.py
@@ -8,8 +8,10 @@ requires = [
     'numpy',
     'ConfigSpace'
     'pandas',  # missing dependency of nasbench
-    'git+https://github.com/google-research/nasbench.git@master'
-    ]
+]
+non_pypi_requires = [
+    'git+https://github.com/google-research/nasbench.git@master#egg=nasbench' 
+]
 
 setup(name='profet',
       version='0.0.1',
@@ -18,4 +20,6 @@ setup(name='profet',
       author_email='kleinaa@cs.uni-freiburg.de',
       keywords='hyperparameter optimization',
       packages=find_packages(),
-      install_requires=requires)
+      install_requires=requires,
+      dependency_links=non_pypi_requires,
+     )
